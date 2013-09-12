144b2abcb986c0d7d0eba7e9c96151537e230b25
cosc301 <cosc301@csvm.(none)>
2013-09-12 00:31:50 -0400

    almost done with sequential

--- main.c
+++ main.c
@@ -11,87 +11,74 @@
 #include <sys/stat.h>
 #include <poll.h>
 #include <signal.h>
-
+#include "proj02.h"
 
 int main(int argc, char **argv) {
-	
-	char* delim = ";";
+	//tokenify("test;bob");
+	sequential();
+	return 0;
+}
+
+void sequential () { //use if sequential is called
 	char* cmt = "#";
+	int exitSwitch = 0;
 	pid_t pid;
-	int seq = 0;
-	int numcommands = 0;
+	int par = 0;
 	int i = 0;
 	printf("%s","type here:  ");
 	fflush(stdout);
 	char buffer[1024];
-	char* token;
 	char* cmttoken;
-	char *cmd[] = { "/bin/ls", "-ltr", ".", NULL };
+	//char *cmd[] = { "/bin/ls", "-ltr", ".", NULL };
+	char **linefinal;
 	while (fgets(buffer, 1024, stdin) !=NULL) {
 		cmttoken = strtok(buffer, cmt); //Nothing included after first '#'
-		numcommands = 0;
-		for(i=0;cmttoken[i]!=NULL;i++){
-			if (cmttoken[i]==';')
-				numcommands++;
+		linefinal = tokenify(cmttoken); 
+		
+
+		if ((pid=fork())<0) { //if fork fail, error
+			perror("Fork ERROR"); //should we exit
 		}
-		i=0;
-		char **commands[numcommands+1];
-		token = strtok(cmttoken, delim); //cmttoken is strictly the non-commented code
-		commands[i]=token;
-		//assume parallel unless user switches out
-		//if not one of our speciall instructions, fork
-		//in fork, update the cmd array to be token (user instruction)
-		//execv
-		while (token!=NULL){ //itterate the line sepearted by ;
-			printf("%s","current token:  ");
-			printf("%s\n",token);
-			//if exit, exit
-			if (strncmp(token,"exit\n",4)==0) {
-				exit(0);
+		else if (pid==0) { //if child, execv
+			printf("%s\n","I Forked");
+			while (linefinal[i]!=NULL){ //itterate the line sepearted by ;
+				printf("%s","current token:  ");
+				printf("%s\n",linefinal[i]);
+				//if exit, exit
+				if (strncmp(linefinal[i],"exit\n",4)==0) {
+					//exit(0);
+					exitSwitch = 1;
+				}
+
+				//mode parallel, call new function
+				else if (strncmp(linefinal[i],"mode parallel",14)==0) {
+					par = 1;
+				}
+				else if (strncmp(linefinal[i],"mode p",6)==0) {
+					par = 1;
+				}
+				else{
+					execv(linefinal[i],linefinal);
+					printf("%s","test   :");
+					printf("%s\n",linefinal[i]);
+				}
+				i++;
 			}
 
-			//mode sequential, call new function
-			else if (strncmp(token,"mode sequential",14)==0) {
-				seq = 1;
-			}
-			else if (strncmp(token,"mode s",6)==0) {
-				seq = 1;
-			}
-
-			//if mode, print mode type 
-			//mode parallel, call new function
-			/*if (strncmp(token,"mode parallel", 13)==0) {
-				parallel(buffer);
-			}
-			else if (strncmp(token,"mode p",6)==0) {
-				parallel();
-			}*/	
-			else {
-				if ((pid=fork())==-1) { //if fork fail, error
-					perror("Fork ERROR");
-				}
-				if (pid==0) { //if child, execv
-					printf("%s\n","I Forked");
-					strcpy(cmd[1],token);
-					execv(cmd[0],cmd);
-				}
-			}
-			i++;
-			token = strtok(NULL, delim); //next token in line
-			commands[i]=token;
 		}
-		if (seq == 1) { //if sequential acctivated, go to sequential
-			sequential();
+		else {
+			int wc = wait(NULL);
+		}
+		//parent wait
+		if (exitSwitch == 1) {
+			exit(0);
+		}
+		if (par == 1) { //if sequential acctivated, go to parallel
+			parallel();
 		}
 		printf("%s","type here:  ");
 		fflush(stdout);		
 	}
-
-	return 0;
-}
-
-void sequential (char* token) { //use if sequential is called
-	printf("%s\n","mode sequential indeed");
 
 	
 }
@@ -100,4 +87,27 @@ void parallel (char* currentLine) { //use if parallel is called
 
 	printf("%s\n","mode parallel indeed");
 	
+}
+
+char **tokenify(const char *string) {
+	const char *sep = ";";
+	char *tmp, *word;
+	char *s = strdup(string);
+	int num_tokens = 0;
+	for(word=strtok_r(s,sep,&tmp);word != NULL;word=strtok_r(NULL,sep,&tmp)){
+		printf("%s\n",word);
+		num_tokens++;		
+	}
+	
+	char ** final = (char**)malloc((num_tokens+1) * sizeof(char*));
+        int i = 0;
+
+	for(word=strtok_r(s,sep,&tmp);word != NULL;word=strtok_r(NULL,sep,&tmp)){
+                final[i++] = strdup(word);            
+        }
+
+	final[i] = NULL;
+	free(s);
+
+	return final;			
 }
