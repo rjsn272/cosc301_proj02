bfaee58cd9713c6ac985ac6ccd68cc580241148f
cosc301 <cosc301@csvm.(none)>
2013-09-30 16:24:45 -0400

    updated

--- main.c
+++ main.c
@@ -13,12 +13,26 @@
 
 
 int main(int argc, char **argv) {
-
+	char* delim = ";";
+	int comment = 0;
 	printf("%s","type here:  ");
 	fflush(stdout);
 	char buffer[1024];
+	char* token;
+	printf("%s\n","testing");
 	while (fgets(buffer, 1024, stdin) !=NULL) {
-		printf("%s\n","test");
+		printf("%s\n","1");
+		token = strtok(buffer, delim);
+		printf("%s\n","2");
+		while (token!=NULL && comment!=1){ //itterate the line sepearted by ;
+						printf("%s\n",token);
+			if (strchr(token,"#") ==0){ //if comment, break
+					comment = 1;
+					printf("%s\n","HERE!!!");
+			}		
+
+			token = strtok(NULL, delim);
+		}
 	}
 
 	return 0;
