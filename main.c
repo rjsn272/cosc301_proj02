48f4860d690557e91f1e1837628dc3ead3cd8c58
cosc301 <cosc301@csvm.(none)>
2013-10-08 20:41:01 -0400

    correct format

--- main.c
+++ main.c
@@ -14,84 +14,90 @@
 #include "proj02.h"
 
 int main(int argc, char **argv) {
-	sequential();
-	return 0;
-}
-
-void sequential () { //use if sequential is called
 	char* cmt = "#";
 	int exitSwitch = (int)malloc(sizeof(int));
 	exitSwitch=0;
-	pid_t pid;
+
 	int par = 0;
+	int seq = 1;
 	int i = 0;
 	printf("%s","type here:  ");
 	fflush(stdout);
 	char buffer[1024];
 	char* cmttoken;
 	//char *cmd[] = { "/bin/ls", "-ltr", ".", NULL };
-	char **linefinal;
+	char **linefinal= (char **)malloc(sizeof(char**));
 	while (fgets(buffer, 1024, stdin) !=NULL) {
 		cmttoken = strtok(buffer, cmt); //Nothing included after first '#'
 		linefinal = tokenify(cmttoken); 
-		
+
 		//while loop to check exit and mode switch
 		while (linefinal[i]!=NULL){ //itterate the line sepearted by ;
-				if (strncmp(linefinal[i],"exit",3)==0) {
-					exitSwitch = 1;
-				}
-
-				//mode parallel, call new function
-				else if (strncmp(linefinal[i],"mode parallel",14)==0) {
-					par = 1;
-				}
-				else if (strncmp(linefinal[i],"mode p",6)==0) {
-					par = 1;
-				}
-				i++;
-		}
-
-		if ((pid=fork())<0) { //if fork fail, error
-			perror("Fork ERROR"); //should we exit
-		}
-		else if (pid==0) { //if child, execv
-			printf("%s\n","I Forked");
-			while (linefinal[i]!=NULL){ //itterate the line sepearted by ;
-				printf("%s","current token:  ");
-				printf("%s\n",linefinal[i]);
-				//ask Sommers if this if is OK
-				if (strncmp(linefinal[i],"exit",3)==0) {
-				}
-				else if (strncmp(linefinal[i],"mode parallel",14)==0) {
-				}
-				else if (strncmp(linefinal[i],"mode p",6)==0) {
-				}
-				else{
-					execv(linefinal[i],linefinal);
-					printf("%s","test   :");
-					printf("%s\n",linefinal[i]);
-				}
-				i++;
+			if (strncmp(linefinal[i],"exit",3)==0) {
+				exitSwitch = 1;
 			}
 
-		}
-		else if (pid>0) {
-			int wc = wait(NULL); 	//parent wait
-			printf("%s\n","got to parent");
-			if (par == 1) { //if sequential acctivated, go to parallel
-				parallel();
+			//mode parallel, call new function
+			else if (strncmp(linefinal[i],"mode parallel",14)==0) {
+				par = 1;
 			}
+			else if (strncmp(linefinal[i],"mode p",6)==0) {
+				par = 1;
+			}
+			else if (strncmp(linefinal[i],"mode sequential",14)==0) {
+				seq = 1;
+			}
+			else if (strncmp(linefinal[i],"mode s",6)==0) {
+				seq = 1;
+			}
+			i++;
+		}
+		if (seq == 1) {
+			sequential(linefinal);
+		}
+		if (par ==1){
+			parallel();
 		}
 
-		printf("%s","exit switch: ");
-		printf("%d\n",exitSwitch);
-		if (exitSwitch == 1) {
-			exit(0);
-		}
 
-		fflush(stdout);		
 	}
-	free(exitSwitch);
+	return 0;
+}
+
+void sequential (char** linefinal) { //use if sequential is called
+	int i = 0;
+	printf("%s\n","got to seq");
+	pid_t pid;
+	if ((pid=fork())<0) { //if fork fail, error
+		perror("Fork ERROR"); //should we exit
+	}
+	//child
+	else if (pid==0) { //if child, execv
+		printf("%s\n","I Forked");
+		while (linefinal[i]!=NULL){ //itterate the line sepearted by ;
+			printf("%s","current token:  ");
+			printf("%s\n",linefinal[i]);
+			//ask Sommers if this if is OK
+			if (strncmp(linefinal[i],"exit",3)==0) {
+			}
+			else if (strncmp(linefinal[i],"mode parallel",14)==0) {
+			}
+			else if (strncmp(linefinal[i],"mode p",6)==0) {
+			}
+			else{
+				//execv(linefinal[i],linefinal);
+				printf("%s","test   :");
+				printf("%s\n",linefinal[i]);
+			}
+			i++;
+		}
+
+	}
+	//parent
+	else (pid>0) {
+		int wc = wait(NULL); 	//parent wait
+		printf("%s\n","got to parent");
+	}
 	
 }
 
