ae4bcdc062072f41fd18bf5e17f6f1e5069cdd4b
cosc301 <cosc301@csvm.(none)>
2013-10-11 14:33:47 -0400

    almost done

--- main.c
+++ main.c
@@ -26,23 +26,33 @@ void list_insert(const char* path, struct node **head) {
 	*head = newnode;
 }
 
+
+void list_insertPid(pid_t pid, struct node **head, const char* command) {
+	struct node *newnode = malloc(sizeof(struct node));
+	int pidFinal = (int)malloc(sizeof(pid));
+	char* cmdFinal;
+	cmdFinal = strdup(command);
+	pidFinal = pid;
+	newnode->pid=pidFinal;
+	newnode->cmd = cmdFinal;
+
+	newnode->next=*head;
+	*head = newnode;
+}
+
 void readFile(struct node **head) {
 	char* line;
 	struct node *temp = NULL;
 	temp=*head;
-	char* lineTemp;
 	size_t numberBytes=NULL;
 	FILE *datafile;
-	int stringLength = 0;
 	ssize_t read;
-	int i = 0;
 	datafile = fopen("shell-config.txt","r");
  	if (datafile == NULL) {
                 printf("%s\n","Unable to open file");
                 exit(-1);
         }
 	while(read = getline(&line,&numberBytes, datafile)!=-1) { ///loop to read file 
-		//line = tokenify(line,"\n");
 		line[strlen(line)-1]='\0';
 		list_insert(line,&temp);
 		free(line);
@@ -53,68 +63,170 @@ void readFile(struct node **head) {
 }
 
 int main(int argc, char **argv) {
+	//exit wont exit
+	//pid print on completetion is 0 and not pid
+	//prints prompt to often
+	//need to add the following commands: jobs, paus PID, resume PID
 	char* cmt = "#";
+	char* cmdToPrint;
 	int exitSwitch = (int)malloc(sizeof(int));
 	exitSwitch=0;
 	int seq = 1;
+	int status;
 	int i = 0;
-
+	pid_t parentPid= (pid_t)malloc(sizeof(pid_t)); 
+	int pidArray; 
+	int run = 1;
 	char buffer[1024];
 	char* cmttoken;
 	char **paths = (char **)malloc(sizeof(char**));
 	char **linefinal= (char **)malloc(sizeof(char**));
 	struct node *head = NULL;
+	struct node *headPid = NULL;
+	struct node *headPidTemp = NULL;
+	pid_t pidStatus;
 	readFile(&head);	//create linked list
+
+	struct pollfd pfd;
+	pfd.fd = 0;
+	pfd.events = POLLIN;
+	pfd.revents = 0;
+
+	int rv = 0;
+	
 
 	printf("%s","prompt>  "); //print promot
 	fflush(stdout);
+	while (run) {
+	rv = poll(&pfd, 1, 100);
+	//while (fgets(buffer, 1024, stdin) !=NULL) {
+		if (rv == 0) { //if no typing
+			//printf("timeout\n");
+		}
+		else if (rv>0) { //if type something
+			printf("%s\n",""); 
+			fgets(buffer, 1024, stdin);
+			pidArray = fork();
+			list_insertPid(pidArray,&headPid,buffer);
+				cmttoken = strtok(buffer, cmt); //Nothing included after first '#'
+				linefinal = tokenify(cmttoken,";");
+			if (pidArray<0) { //if fork fail, error
+				perror("Fork ERROR"); //should we exit
+			}	
+			if (pidArray==0) { //CHILD
+				//execute
 
-	while (fgets(buffer, 1024, stdin) !=NULL) {
-		cmttoken = strtok(buffer, cmt); //Nothing included after first '#'
-		linefinal = tokenify(cmttoken,";");
-		if (seq == 1) {
-			sequential(linefinal,&head);
-		}
-		else{
-			parallel(linefinal,&head);
-		}
-	
-		//while loop to check exit and mode switch after everything has run, commented out exitSwitch
-		i=0;
-		while (linefinal[i]!=NULL){ //itterate the line sepearted by ;		
-			if (strncmp(linefinal[i],"exit",4)==0) {
-				exitSwitch = 1;
-			}
-			else if (strncmp(linefinal[i],"mode parallel",13)==0||strncmp(linefinal[i],"mode p",6)==0){
-				seq = 0;
-				printf("%s\n","YOU HAVE NOW ENTERED MODE PARALLEL");	
-			}
-			else if (strncmp(linefinal[i],"mode sequential",15)==0 || strncmp(linefinal[i],"mode s",6)==0){
-				seq = 1;
-				printf("%s\n","YOU HAVE NOW ENTERED MODE SEQUENTIAL");
-			}
-			else if (strncmp(linefinal[i],"mode",4)==0) {
-				if (seq == 0) {
-					printf("%s\n","Mode: Parallel");
-				}
 				if (seq == 1) {
-					printf("%s\n","Mode:  Sequential");
+
+
+					sequential(linefinal,&head);
 				}
+				else{
+					parallel(linefinal,&head);
+				}
+
+				//while loop to check exit and mode switch after everything has run, commented out exitSwitch
+				i=0;
+				while (linefinal[i]!=NULL){ //itterate the line sepearted by ;		
+					if (strncmp(linefinal[i],"exit",4)==0) {
+						exitSwitch = 1;
+					}
+					else if (strncmp(linefinal[i],"mode parallel",13)==0||strncmp(linefinal[i],"mode p",6)==0){
+						seq = 0;
+						printf("%s\n","YOU HAVE NOW ENTERED MODE PARALLEL");	
+					}
+					else if (strncmp(linefinal[i],"mode sequential",15)==0 || strncmp(linefinal[i],"mode s",6)==0){
+						seq = 1;
+						printf("%s\n","YOU HAVE NOW ENTERED MODE SEQUENTIAL");
+					}
+					else if (strncmp(linefinal[i],"mode",4)==0) {
+						if (seq == 0) {
+							printf("%s\n","Mode: Parallel");
+						}
+						if (seq == 1) {
+							printf("%s\n","Mode:  Sequential");
+						}
+					}
+					i++;
+				}	
+				if (exitSwitch==1){
+					run=0;
+				}
+				printf("%s","prompt>  ");
 			}
-			i++;
-		}	
-		if (exitSwitch==1){
-			exit(0);
+			//else { //parent	
+				//do only once child ends
+				/*i=0;
+				while (linefinal[i]!=NULL){ //itterate the line sepearted by ;	
+					printf("%s","test:   ");
+					printf("%s\n",linefinal[i]);	
+					if (strncmp(linefinal[i],"exit",4)==0) {
+						exitSwitch = 1;
+					}
+					else if (strncmp(linefinal[i],"mode parallel",13)==0||strncmp(linefinal[i],"mode p",6)==0){
+						seq = 0;
+						printf("%s\n","YOU HAVE NOW ENTERED MODE PARALLEL");	
+					}
+					else if (strncmp(linefinal[i],"mode sequential",15)==0 || strncmp(linefinal[i],"mode s",6)==0){
+						seq = 1;
+						printf("%s\n","YOU HAVE NOW ENTERED MODE SEQUENTIAL");
+					}
+					else if (strncmp(linefinal[i],"mode",4)==0) {
+						if (seq == 0) {
+							printf("%s\n","Mode: Parallel");
+						}
+						if (seq == 1) {
+							printf("%s\n","Mode:  Sequential");
+						}
+					}
+					
+					if (exitSwitch==1){
+						run=0;
+					}
+					i++;
+				}*/	
+				//
+				headPidTemp = headPid;
+				while(headPid!=NULL) {	
+					pidStatus = waitpid(headPid->pid, &status, WNOHANG); //parent wait, without hang
+					if (pidStatus!=0) {
+						printf("%s\n","");
+						printf("%s","PROCESS: ");	
+						strcpy(cmdToPrint,headPid->cmd);
+						cmdToPrint[strlen(headPid->cmd)-1]='\0';//strip newline
+						printf("%d",headPid->pid);
+						printf("%s"," (");
+						printf("%s",cmdToPrint);
+						printf("%s\n",") COMPLETED");
+						printf("%s","prompt> ");
+
+						break;
+					}
+					if (headPid->next==NULL) {
+						headPid=headPidTemp;
+					}
+					headPid=headPid->next;
+				}
+				headPid = headPidTemp;
+			
+			//}
+			fflush(stdout);
+
 		}
-		fflush(stdout);
-		printf("%s","prompt>  ");
+		else {
+			//printf("there was some kind of error: %s\n",strerro(errno));
+		}
 	}
 	free(linefinal);
 	free(paths);
+	free(parentPid);
+	free(exitSwitch);
+	free(cmdToPrint);
 	return 0;
 }
 
-void sequential (char** linefinal,struct node **head) { //use if sequential is called		
+void sequential (char** linefinal,struct node **head) { //use if sequential is called			
+		
 	struct node *temp = NULL;
 	temp=*head;
 	int foundFile = -1;
@@ -126,7 +238,6 @@ void sequential (char** linefinal,struct node **head) { //use if sequential is c
 	char ** arrayToExec = NULL;
 	pid_t pidArray[sizeof(linefinal)];
 	while (linefinal[i]!=NULL){
-		printf("%s\n",linefinal[i]);
 		if (isBuiltIn(linefinal[i])==0){
 			arrayToExec = tokenify(linefinal[i],"\n,\t, ");
 			if (arrayToExec[0][0]=='/') { //if given path
@@ -155,7 +266,6 @@ void sequential (char** linefinal,struct node **head) { //use if sequential is c
 				}	
 				if (pidArray[i]==0) {
 					if (execv(arrayToExec[0], arrayToExec) < 0) {
-
 						fprintf(stderr, "execv failed: %s\n", strerror(errno));
 						exit(0);
 					}
@@ -168,7 +278,10 @@ void sequential (char** linefinal,struct node **head) { //use if sequential is c
 		execute = 0;
 		i++;
 	}
+	i = 0;
+
 	free(fileTempPath);
+	free(arrayToExec);
 		
 }
 
@@ -224,7 +337,6 @@ void parallel (char** linefinal, struct node **head) { //use if parallel is call
 
 		}
 		execute = 0;
-
 		i++;
 	}
 	i = 0;
--- proj02.h
+++ proj02.h
@@ -3,8 +3,12 @@
 
 struct node {
     const char* path;
+	const char* cmd;
+	pid_t pid;
     struct node *next; 
 };
+
+
 
 void list_insert(const char* path, struct node **head);
 void readFile(struct node **head);
@@ -13,4 +17,4 @@ void sequential(char** linefinal, struct node **head);
 char **tokenify(const char *string, const char *delim);
 int isBuiltInt(char* linefinal);
 
-#endif // __LAB02_H__
+#endif // __PROJ02_H__
